804c73d9f47361e1d6eb58583881e72a509ad464
robincox <mail.robin.cox@gmail.com>
2017-03-27 17:12:42 +0200

    starting project

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/endTurn")
+    @SendTo("/topic/gameRoom")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
