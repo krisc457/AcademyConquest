1b5f75c380dc97c1dd09104c9ada8f332ace0f19
PatrikRiku <patrik@riku.se>
2017-03-28 16:24:28 +0200

    html5 and map

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,10 +3,25 @@ package hello;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
 
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelmodel = new ModelAndView("index");
+        return modelmodel;
+    }
+
+    @GetMapping("/map")
+    public ModelAndView map(){
+        ModelAndView modelmodel = new ModelAndView("map");
+        return modelmodel;
+    }
 
     @MessageMapping("/endTurn")
     @SendTo("/topic/gameRoom")
