3f9eeaea80ccd10b91376b7dabe6d1136ad978f0
robincox <mail.robin.cox@gmail.com>
2017-03-29 09:56:21 +0200

    Klickbara länder med modal

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,10 +3,17 @@ package hello;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
+    @GetMapping("/map")
+    public ModelAndView map() {
+        ModelAndView mapmodel = new ModelAndView("map");
+        return mapmodel;
+    }
 
     @MessageMapping("/endTurn")
     @SendTo("/topic/gameRoom")
