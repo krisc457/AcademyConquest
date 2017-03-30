90bff4ae4e8b7d6346f7134b4176807d88f49d7d
krisc457 <kristoffer.schmidt@hotmail.com>
2017-03-30 15:21:52 +0200

    Changed the Regon class

--- src/main/java/hello/Player.java
+++ src/main/java/hello/Player.java
@@ -8,6 +8,46 @@ public class Player {
     private String userName;
     private String majorNation;
     private int[] countriesOwned;
+    private int treasury;
+    private boolean fortification;
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMajorNation() {
+        return majorNation;
+    }
+
+    public void setMajorNation(String majorNation) {
+        this.majorNation = majorNation;
+    }
+
+    public int[] getCountriesOwned() {
+        return countriesOwned;
+    }
+
+    public void setCountriesOwned(int[] countriesOwned) {
+        this.countriesOwned = countriesOwned;
+    }
+
+    public int getTreasury() {
+        return treasury;
+    }
+
+    public void setTreasury(int treasury) {
+        this.treasury = treasury;
+    }
+
+    public boolean isFortification() {
+        return fortification;
+    }
+
+    public void setFortification(boolean fortification) {
+        this.fortification = fortification;
+    }
 }
