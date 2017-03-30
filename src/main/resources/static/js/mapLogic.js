c29778bd5beea5c3b0887f50fcb8afb21599a0bd
robincox <mail.robin.cox@gmail.com>
2017-03-30 17:12:40 +0200

    grouping

--- src/main/resources/static/js/mapLogic.js
+++ src/main/resources/static/js/mapLogic.js
@@ -14,12 +14,17 @@ $(document).ready(function() {
     $("#zoomIn").click(function(e){
         var newWidth = width * 1.25;
         var newHeight = height * 1.25;
+        var scrolledX = window.pageXOffset;
+        var scrolledY = window.pageYOffset;
+        $("#gameMap").css("transform-origin", 0 + "px" + scrolledY + "px");
         $("#gameMap").css("animation", "zoomIn 0.5s");
-        $("#gameMap").css("animation", "zoomIn 0.5s");
+        /*
         setTimeout(function(){
             $("#gameMap").css("width", newWidth + "px");
             $("#gameMap").css("height", newHeight + "px");
+            window.scrollTo(scrolledX * 1.25, scrolledY * 1.26);
         }, 500);
+        */
     });
 
     $("#zoomOut").click(function(e){
